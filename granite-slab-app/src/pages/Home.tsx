49e6bfce0d1dc1963ee57ce09ea87b4be3ff4838
David Cibin <ddcibin@gmail.com>
2025-06-02 19:05:27 -0400

    add mock login

--- granite-slab-app/src/pages/Home.tsx
+++ granite-slab-app/src/pages/Home.tsx
@@ -1,8 +1,30 @@
 export default function Home() {
   return (
     <div className="flex flex-col gap-3 items-center justify-center h-screen">
-      <div className="text-2xl">Home</div>
-      <button className="btn-primary">Click me</button>
+      <div>
+        <div className="flex items-center gap-5 mb-3">
+          <label htmlFor="name-filter">Username</label>
+          <input
+            id="name-filter"
+            type="text"
+            onChange={(e) => console.log(e.target.value)}
+            className="w-full mb-2 border-b focus:border-teal-500 focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center gap-5 mb-10">
+          <label htmlFor="name-filter">Password</label>
+          <input
+            id="name-filter"
+            type="text"
+            onChange={(e) => console.log(e.target.value)}
+            className="w-full mb-2 border-b focus:border-teal-500 focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center justify-around gap-5 mb-3">
+          <button className="btn-primary">Login</button>
+          <button className="btn-primary">Register</button>
+        </div>
+      </div>
     </div>
   );
 }
